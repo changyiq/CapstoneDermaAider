463424fca7b6a4945630c89a2864e02e2866962c
Yiqian Chang <changyiq@sheridancollege.ca>
2021-12-03 12:17:36 -0500

    deleted a unused comment in loginActivity

--- app/src/main/java/project/capstone6/acne_diagnosis/LoginActivity.java
+++ app/src/main/java/project/capstone6/acne_diagnosis/LoginActivity.java
@@ -1,18 +1,8 @@
 package project.capstone6.acne_diagnosis;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.biometric.BiometricPrompt;
-import androidx.core.content.ContextCompat;
-
 import android.content.DialogInterface;
 import android.content.Intent;
-
-
 import android.os.Bundle;
-
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
@@ -21,12 +11,19 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
+
 import com.facebook.AccessToken;
 import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
-
+import com.facebook.FacebookSdk;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.Auth;
@@ -40,14 +37,12 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
-import com.facebook.FacebookSdk;
 
 import java.util.concurrent.Executor;
 
@@ -322,8 +317,6 @@ public class LoginActivity extends AppCompatActivity {
         });
 
     }
-    // -----------------------------------Fingerprint Login ---------------------------------
-
 
     @Override
     public void onStart() {
@@ -333,7 +326,6 @@ public class LoginActivity extends AppCompatActivity {
         if (currentUser != null) {
             updateUI(currentUser);
         }
-
     }
 
     // check activity result
